4f5a8c87dff079f1e895558504bd6de637c56de6
Joel John <joeljohn5112@gmail.com>
2023-01-28 22:20:46 +0530

    weather api url get request added

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const express = requir("express");
+const express = require("express");
+const https = require("https");
 const app = express();
 
+app.get("/", function (req, res) {
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=1edfabf03549997d11e05eb35eae1e4b&units=metric";
+  https.get(url, function (response) {
+    console.log(response);
+  });
+  res.send("Server is up and running");
+});
 app.listen(3000, function () {
   console.log("Server is running on 3000");
 });
