6dc1151bbd57661bdaae28f85aee85fc77942dd2
Joel John <joeljohn5112@gmail.com>
2023-01-29 11:20:12 +0530

    JSON parsed

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ app.get("/", function (req, res) {
   const url =
     "https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=1edfabf03549997d11e05eb35eae1e4b&units=metric";
   https.get(url, function (response) {
-    console.log(response);
+    console.log(response.statusCode);
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+      console.log(weatherData.main.temp);
+      console.log(weatherData.weather[0].description);
+    });
   });
   res.send("Server is up and running");
 });
